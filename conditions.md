cf9f43e033ba71d2e4b1ced541ff4eea208ccb83
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-06-17 17:02:32 -0500

    Revert "Formated all documents"
    This reverts commit dba8f3036ddef67fd7616e2192bd4129ef5ad7ac.

--- conditions.md
+++ conditions.md
@@ -1,47 +1,35 @@
 # Conditions
-
-This will show you the different conditions that you can encounter is Simple Weather.
+This will show you the diforent conditions that you can encounter is Simple Weather.
 
 ### Hot
-
 When the temp is over 90
 
 ### Cold
-
 When the temp is under 10
 
 ### Foggy
-
 When the visibility is under 40%
 
 ### Windy
-
 When the wind speed is over 25 MPH and it is not Hot, Cold, or foggy
 
 ### Cloudy
-
-When it is not hot, cold, and has a >90% cloud coverage
+When it is not hot, cold, and has a >90% cloud coverige
 
 ### Partly Cloudy
-
-When it is cloudy but, under 90% cloud coverage
+When it is cloudy but, under 90% cloud coverige
 
 ### Sunny
-
 When the API shows "Sun"
 
 ### Raining
-
 When the API shows "Rain"
 
 ### Snowing
-
 When the API shows "Snow"
 
 ### Bad
-
 When the API shows extreme weather.
 
 ### Anything else
-
 The algorithm has nothing, it will show the API's description.
--- main.js
+++ main.js
@@ -1,188 +1,161 @@
 // Copyright (c) Jacob Drath 2022
 
-if (navigator.onLine == false) {
-  Toastify({
-    text: "No internet connection",
-    style: {
-      background:
-        "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
-    },
-    gravity: "bottom",
-  }).showToast();
+if (navigator.onLine==false) {
+    Toastify({
+        text: "No internet connection",
+        style: {
+          background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
+        },
+        gravity: "bottom"
+      }).showToast();
 }
 
-if (localStorage.getItem("lat") == null) {
-} else {
-  getWX();
+
+if (localStorage.getItem("lat")==null) {} else {
+    getWX()
 }
+
 
 function setZIP() {
-  document.getElementById("zipr").innerHTML =
-    "<img src='load.gif' width='40px'>Please wait...";
-  fetch(
-    "https://api.openweathermap.org/geo/1.0/zip?zip=" +
-      document.getElementById("zip").value +
-      "," +
-      document.getElementById("zip-c").value +
-      "&appid=b4b150ac011d2c689bb0960425153055"
-  )
-    .then((response) => response.json())
-    .then((data) => handleZIP(data));
+    document.getElementById("zipr").innerHTML=("<img src='load.gif' width='40px'>Please wait...")
+    fetch('https://api.openweathermap.org/geo/1.0/zip?zip='+document.getElementById("zip").value+','+document.getElementById("zip-c").value+'&appid=b4b150ac011d2c689bb0960425153055')
+        .then(response => response.json())
+        .then(data => handleZIP(data))
 }
 function handleZIP(e) {
-  if (e.name == undefined) {
-    document.getElementById("zipr").innerHTML =
-      "<p style='color: red;'>ZIP Code is invalid</p>";
-    Toastify({
-      text: "ZIP code is invalid.",
-      style: {
-        background:
-          "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
-      },
-      gravity: "bottom",
-    }).showToast();
-  } else {
-    localStorage.setItem("unit", document.getElementById("unit-b").value);
-    localStorage.setItem("long", e.lon);
-    localStorage.setItem("lat", e.lat);
-    document.getElementById("zipr").innerHTML =
-      "<p>Location was saved as " +
-      e.name +
-      ". Weather will open shortly... <img src='load.gif' width='40px'></p>";
-    Toastify({
-      text: "Location was saved. Please wait...",
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-      gravity: "bottom",
-    }).showToast();
-    setTimeout(function () {
-      window.open(window.location.href, "_self");
-    }, 2000);
-  }
+    if (e.name==undefined) {
+        document.getElementById("zipr").innerHTML=("<p style='color: red;'>ZIP Code is invalid</p>")
+        Toastify({
+            text: "ZIP code is invalid.",
+            style: {
+              background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
+            },
+            gravity: "bottom"
+          }).showToast();
+    } else {
+        localStorage.setItem("unit", document.getElementById("unit-b").value)
+        localStorage.setItem("long", e.lon)
+        localStorage.setItem("lat", e.lat)
+        document.getElementById("zipr").innerHTML=("<p>Location was saved as "+e.name+". Weather will open shortly... <img src='load.gif' width='40px'></p>")
+        Toastify({
+            text: "Location was saved. Please wait...",
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            gravity: "bottom"
+          }).showToast();
+        setTimeout(function(){
+            window.open(window.location.href, "_self")
+        }, 2000);
+    }
 }
 
 function setGPS() {
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      
+      function success(pos) {
+        var crd = pos.coords;
+      
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
 
-  function success(pos) {
-    var crd = pos.coords;
-
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-
-    localStorage.setItem("long", crd.longitude);
-    localStorage.setItem("lat", crd.latitude);
-    //localStorage.setItem("unit", document.getElementById("unit-a").value)
-    getWX();
-  }
-
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-    alert(
-      "There was an error getting your location. Please make sure it is on and try again."
-    );
-    Toastify({
-      text: "There was an error getting your location. Please make sure it is on and try again.",
-      style: {
-        background:
-          "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
-      },
-      gravity: "bottom",
-    }).showToast();
-  }
-
-  navigator.geolocation.getCurrentPosition(success, error, options);
+        localStorage.setItem("long",crd.longitude)
+        localStorage.setItem("lat",crd.latitude)
+        //localStorage.setItem("unit", document.getElementById("unit-a").value)
+        getWX()
+      }
+      
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        alert("There was an error getting your location. Please make sure it is on and try again.")
+        Toastify({
+            text: "There was an error getting your location. Please make sure it is on and try again.",
+            style: {
+              background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))",
+            },
+            gravity: "bottom"
+          }).showToast();
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
 }
 
-function getWX() {
-  document.body.innerHTML =
-    "<center><h1>Please wait... <img src='load.gif' width='40px'></h1></center>";
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?units=" +
-      localStorage.getItem("unit") +
-      "&lat=" +
-      localStorage.getItem("lat") +
-      "&lon=" +
-      localStorage.getItem("long") +
-      "&appid=b4b150ac011d2c689bb0960425153055"
-  )
-    .then((response) => response.json())
-    .then((data) => showWX(data));
+
+function getWX () {
+    document.body.innerHTML=("<center><h1>Please wait... <img src='load.gif' width='40px'></h1></center>")
+    fetch('https://api.openweathermap.org/data/2.5/weather?units='+localStorage.getItem("unit")+'&lat='+localStorage.getItem("lat")+'&lon='+localStorage.getItem("long")+'&appid=b4b150ac011d2c689bb0960425153055')
+        .then(response => response.json())
+        .then(data => showWX(data))
 }
+
 
 function showWX(w) {
-  var l = w.name;
-  var temp = Math.floor(w.main.temp);
-  if (localStorage.getItem("icon-show") == "1") {
-    icon =
-      "<img src='http://openweathermap.org/img/wn/" +
-      w.weather[0].icon +
-      "@2x.png' width='60px'><br>";
-  } else {
-    icon = "";
-  }
-  if (temp < 10) {
-    if (localStorage.getItem("unit") == "imperial") {
-      configS(l, "cold", temp, icon);
-    }
-  }
-
-  if (temp > 95) {
-    if (localStorage.getItem("unit") == "imperial") {
-      configS(l, "hot", temp, icon);
-    }
-  } else if (w.weather[0].main == "Rain") {
-    configS(l, "raining", temp, icon);
-  } else if (w.weather[0].main == "Snow") {
-    configS(l, "snowing", temp, icon);
-  } else if (w.visibility < 4000) {
-    configS(l, "foggy", temp, icon);
-  } else if (w.wind.speed > 25) {
-    configS(l, "windy", temp, icon);
-  } else if (w.weather[0].main == "Clouds") {
-    configS(l, "cloudy", temp, icon);
-    if (w.clouds.all < 90) {
-      configS(l, "partly cloudy", temp, icon);
-    }
-  } else if (w.weather[0].description == "clear sky") {
-    if (Math.round(new Date().getTime() / 1000) < w.sys.sunset) {
-      configS(l, "sunny", temp, icon);
+    var l = w.name
+    var temp = Math.floor(w.main.temp)
+    if (localStorage.getItem("icon-show")=="1") {
+        icon = "<img src='http://openweathermap.org/img/wn/"+w.weather[0].icon+"@2x.png' width='60px'><br>"
     } else {
-      configS(l, "dark", temp, icon);
+        icon = ""
     }
-  } else if (w.weather[0].main == "Extreme") {
-    configS(l, "bad", temp, icon);
-  } else {
-    configS(l, w.weather[0].description, temp, icon);
-  }
+    if (temp<10) {
+        if (localStorage.getItem("unit")=="imperial") {
+            configS(l, "cold", temp, icon)
+        }
+    }
 
-  if (Math.round(new Date().getTime() / 1000) > w.sys.sunset) {
-    document.body.style.background = "black";
-    document.body.style.color = "white";
-  }
+    if (temp>95) {
+        if (localStorage.getItem("unit")=="imperial") {
+            configS(l, "hot", temp, icon)
+        }
+    }
+    else if (w.weather[0].main=="Rain") {
+        configS(l, "raining", temp, icon)
+    } else if (w.weather[0].main=="Snow") {
+        configS(l, "snowing", temp, icon)
+    } else if (w.visibility<4000) {
+        configS(l, "foggy", temp, icon)
+    } else if (w.wind.speed>25) {
+        configS(l, "windy", temp, icon)
+    } else if (w.weather[0].main=="Clouds") {
+        configS(l, "cloudy", temp, icon)
+        if (w.clouds.all<90) {
+            configS(l, "partly cloudy", temp, icon)
+        }
+    } else if (w.weather[0].description=="clear sky") {
+        if (Math.round((new Date()).getTime() / 1000)<w.sys.sunset) {
+            configS(l, "sunny", temp, icon)
+        } else {
+            configS(l, "dark", temp, icon)
+        }
+    } else if (w.weather[0].main=="Extreme") {
+        configS(l, "bad", temp, icon)
+    } else {
+        configS(l, w.weather[0].description, temp, icon)
+    }
+
+
+    if (Math.round((new Date()).getTime() / 1000)>w.sys.sunset) {
+        document.body.style.background=("black")
+        document.body.style.color=("white")
+    }
 }
+
 
 function configS(loca, cond, temp, icon) {
-  document.body.innerHTML =
-    "<div>Current Location: " +
-    loca +
-    '. <a href="javascript:setGPS()">Reset</a></div><div class="container"><div class="center">     <img src="Logo.png" width="15px"><span style="font-size:15px;margin: 0;">Simple Weather</span><br>' +
-    icon +
-    '<h1 style="margin-top: 2px;">It is ' +
-    cond +
-    '.</h1><p>Project by <a href="https://zzz.jacobdrath.co">Jacob Drath</a>.<br><a id="settings" href="settings">Settings</a></div></div>';
+    document.body.innerHTML=('<div>Current Location: '+loca+ '. <a href="javascript:setGPS()">Reset</a></div><div class="container"><div class="center">     <img src="Logo.png" width="15px"><span style="font-size:15px;margin: 0;">Simple Weather</span><br>'+icon+'<h1 style="margin-top: 2px;">It is '+cond+'.</h1><p>Project by <a href="https://zzz.jacobdrath.co">Jacob Drath</a>.<br><a id="settings" href="settings">Settings</a></div></div>')
 
-  // iOS config
-  var a = location.href;
-  var b = a.substring(a.indexOf("?") + 1);
-  if (b == "ios=true") {
-    document.getElementById("settings").style.display = "none";
-  }
+    // iOS config
+    var a = location.href; 
+    var b = a.substring(a.indexOf("?")+1);
+    if (b=="ios=true") {
+        document.getElementById("settings").style.display=("none")
+    }
 }
+
+
