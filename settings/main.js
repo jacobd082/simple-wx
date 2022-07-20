609571d1ce31604407a2644513b614df18f697bf
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-07-20 15:52:24 -0500

    Bug Fix

---
+++ settings/main.js
@@ -0,0 +1,40 @@
+document.getElementById("showTemp").checked=(Boolean(Number(localStorage.getItem("showTemp"))))
+function setTemp() {
+  localStorage.setItem("showTemp",Number(document.getElementById("showTemp").checked))
+  Toastify({
+          text: "Saved!",
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          gravity: "bottom"
+        }).showToast();
+}
+
+document.getElementById("icon").checked=(Boolean(Number(localStorage.getItem("icon-show"))))
+function setIcon() {
+  localStorage.setItem("icon-show",Number(document.getElementById("icon").checked))
+  Toastify({
+          text: "Saved!",
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          gravity: "bottom"
+        }).showToast();
+}
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
